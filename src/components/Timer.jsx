c5f32a6490330764c2b8b08357814bef3c9fe57d
Sid <sid.mallick1947@gmail.com>
2024-10-06 11:32:03 +0530

    Add working mechanism of start/pause buttons and stop buttons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
+
 const startingvalue = 70;
+
 export default function Timer() {
+
   const [counter, setCounter] = useState(startingvalue);
-  const [runTimer, setRunTimer] = useState(true);
+  const [runTimer, setRunTimer] = useState(false);
 
   function handleControl () {
     setRunTimer(!runTimer);
   }
+  function handleReset () {
+    setRunTimer(false);
+    setCounter(startingvalue);
+  }
 
   useEffect(() => {
-    runTimer && setTimeout(() => {
+    const timer = runTimer && setInterval(() => {
       counter > 0 && setCounter(counter - 1);
-    }, 1000);
+      }, 1000);
+      return () => clearInterval(timer);
   }, [counter,runTimer]);
 
   return (
@@ -20,7 +28,7 @@ export default function Timer() {
       {counter % 60 < 10 ? "0" + (counter % 60) : counter % 60}
       <div id="timer-controls">
         <button onClick={handleControl}>Start/Pause/Resume</button>
-        <button>Reset/Stop</button>
+        <button onClick={handleReset}>Reset/Stop</button>
       </div>
     </div>
   );
