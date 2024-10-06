63ecf5caf464e3f231adeb3f8ad9ca772b2bf1b7
Sid <sid.mallick1947@gmail.com>
2024-10-06 11:07:52 +0530

    Add button to handle starting and stopping timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from "react";
 const startingvalue = 70;
 export default function Timer() {
   const [counter, setCounter] = useState(startingvalue);
+  const [runTimer, setRunTimer] = useState(true);
+
+  function handleControl () {
+    setRunTimer(!runTimer);
+  }
 
   useEffect(() => {
-    setTimeout(() => {
+    runTimer && setTimeout(() => {
       counter > 0 && setCounter(counter - 1);
     }, 1000);
-  }, [counter]);
+  }, [counter,runTimer]);
 
   return (
     <div id="timer">
       {Math.floor(counter / 60)} :{" "}
       {counter % 60 < 10 ? "0" + (counter % 60) : counter % 60}
+      <div id="timer-controls">
+        <button onClick={handleControl}>Start/Pause/Resume</button>
+        <button>Reset/Stop</button>
+      </div>
     </div>
   );
 }
